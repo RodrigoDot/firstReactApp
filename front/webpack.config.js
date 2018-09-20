f9366aaeaa2b07ce846511aa43d5cd2d78376974
Rodrigo <rodrigodot42@gmail.com>
2018-09-20 00:31:36 -0300

    Fixed webpack config file and HELLO WORLD is done

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -14,27 +14,31 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
     alias: {
-      modules: __dirname + 'node_modules'
+      modules: __dirname + '/node_modules'
     }
   },
   plugins: [
     new ExtractTextPlugin('app.css')
   ],
-  modules: {
-    loaders:[{
-      test: './js[x]?$',
-      loader: 'babel-loader',
-      exclude: /node_modules/
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['transform-object-rest-spread']
+  module: {
+    loaders: [
+      {
+        test: /.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+        loader: 'file'
       }
-    },{
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
-      test: /\.woff | .woff2 | .ttf | .eot |.svg*.*$/,
-      loader: 'file'
-    }]
+    ]
   }
 }
