dfe4cc15cf9fb551608e37f7f9e331f599a61dbc
Rodrigo <rodrigodot42@gmail.com>
2018-09-20 23:13:27 -0300

    Added Routes and PageHeader

--- front/src/about/about.jsx
+++ front/src/about/about.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
   <div>
-    <h1>About</h1>
+    <PageHeader name='Sobre' pageDescription='Quem somos'/>
+    <p>Sou Rodrigo e estou desenvolvendo essa aplicação</p>
   </div>
 )
--- front/src/template/menu.jsx
+++ front/src/template/menu.jsx
@@ -4,7 +4,7 @@ export default props => (
   <nav className="navbar navbar-inverse">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="#/todos">
           <i className="fa fa-list-ul" aria-hidden="true"></i> TodoApp
         </a>
         <ul className="nav navbar-nav">
