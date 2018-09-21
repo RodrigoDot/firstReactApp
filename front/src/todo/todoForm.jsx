e888a77d0b9e955114cafd35bfe877b3c0ce84b7
Rodrigo <rodrigodot42@gmail.com>
2018-09-21 00:11:52 -0300

    Update Todo layout

--- front/src/todo/todoForm.jsx
+++ front/src/todo/todoForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export default props => (
-  <form className="navbar-form navbar-default" role="add">
-    <div className="form-group">
-      <input type="text" className="form-control" placeholder="Tarefa" />
+  <form className="todoForm" role="form">
+    <div className="col-xs-12 col-sm-9 com-md-9">
+      <input id='description' type="text" className="form-control"
+        placeholder="Tarefa"></input>
     </div>
-    <button type="submit" className="btn btn-primary">
-      <i className="fa fa-plus" aria-hidden="true"></i>
-    </button>
+    <div className="col-xs-12 col-sm-3 com-md-3">
+      <button type="button" className="btn btn-primary">
+        <i className="fa fa-plus"></i>
+      </button>
+    </div>
   </form>
 )
--- front/src/todo/todoList.jsx
+++ front/src/todo/todoList.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 export default props => (
-  <ul className="list-group">
-    <li className="list-group-item">
-      <span className="badge">14</span> Cras justo odio
-    </li>
-    <li className="list-group-item">
-      <span className="badge">2</span> Dapibus ac facilisis in
-    </li>
-    <li className="list-group-item">
-      <span className="badge">1</span> Morbi leo risus
-    </li>
-  </ul>
+  <div className="col-xs-12 col-sm-12 com-md-12">
+    <p role="separator" class="divider"></p>
+    <ul className="list-group">
+      <li className="list-group-item">
+        <span className="badge">14</span> Cras justo odio
+      </li>
+      <li className="list-group-item">
+        <span className="badge">2</span> Dapibus ac facilisis in
+      </li>
+      <li className="list-group-item">
+        <span className="badge">1</span> Morbi leo risus
+      </li>
+    </ul>
+  </div>
 )
