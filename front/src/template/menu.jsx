9e8f879623a03b09e231b2229e6ccb41fae12054
Rodrigo <rodrigodot42@gmail.com>
2018-09-20 22:53:28 -0300

    Added Navbar

---
+++ front/src/template/menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default props => (
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">
+          <i className="fa fa-list-ul" aria-hidden="true"></i> TodoApp
+        </a>
+        <ul className="nav navbar-nav">
+          <li className="">
+            <a href="#/todos">
+              Tarefas <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="">
+            <a href="#/about">
+              About <span className="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+)
