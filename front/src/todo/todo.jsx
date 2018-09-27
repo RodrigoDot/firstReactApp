2e63b5bab0eb7e3a9560c83437d489f6c18ccd34
Rodrigo <rodrigodot42@gmail.com>
2018-09-27 05:57:28 -0300

    Handling events through components

--- front/src/todo/todo.jsx
+++ front/src/todo/todo.jsx
@@ -10,18 +10,31 @@ export default class Todo extends Component {
   constructor(props) {
     super(props)
     this.handleAdd = this.handleAdd.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+
+    this.state = {
+      description: '',
+      lust: []
+    }
   }
 
   handleAdd() {
-    console.log('HUEHEUEHUEHUEHUEUEHUEHEUHEUHEUEHUEHUE')
-    console.log(this)
+    console.log(this.state.description)
+  }
+
+  handleChange(e) {
+    this.setState({
+      ...this.state,
+      description: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' pageDescription='Cadastro'/>
-        <TodoForm handleAdd={this.handleAdd}/>
+        <TodoForm handleAdd={ this.handleAdd } handleChange={ this.handleChange }
+          description={ this.state.description } />
         <TodoList />
       </div>
     )
--- front/src/todo/todoForm.jsx
+++ front/src/todo/todoForm.jsx
@@ -6,7 +6,8 @@ export default props => (
   <div className="todoForm" role="form">
     <Grid cols='12 9 9'>
       <input id='description' className="form-control"
-        placeholder="Tarefa"></input>
+        onChange={props.handleChange}
+        value={props.description} placeholder="Tarefa"></input>
     </Grid>
     <Grid cols='12 3 3'>
       <IconButton style='primary' icon='plus'
