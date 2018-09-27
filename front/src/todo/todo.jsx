8e01e0ee30dbf26796dc5181a627814452631053
Rodrigo <rodrigodot42@gmail.com>
2018-09-27 06:20:09 -0300

    Added ADD method

--- front/src/todo/todo.jsx
+++ front/src/todo/todo.jsx
@@ -2,25 +2,48 @@ import React, {
   Component
 } from 'react'
 
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
   constructor(props) {
     super(props)
     this.handleAdd = this.handleAdd.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleList = this.handleList.bind(this)
+
+    this.handleList()
 
     this.state = {
       description: '',
-      lust: []
+      list: []
     }
   }
 
   handleAdd() {
-    console.log(this.state.description)
+    const description = this.state.description
+
+    axios.post(URL, {
+      description
+    })
+    .then(function(response){
+      console.log(response.data); // ex.: { user: 'Your User'}
+      console.log(response.status); // ex.: 200
+    });
   }
+
+  // handleList() {
+  //   axios.get(URL)
+  //   .then(function(response){
+  //     console.log(response.data); // ex.: { user: 'Your User'}
+  //     console.log(response.status); // ex.: 200
+  //   });
+  // }
 
   handleChange(e) {
     this.setState({
